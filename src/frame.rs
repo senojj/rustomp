87ed6f04818c6bf00adb27804db03ac894f1c40a
Joshua Jones <joshua.jones@dynata.com>
2020-03-06 09:50:47 -0500

    get encoding right

--- src/frame.rs
+++ src/frame.rs
@@ -49,7 +49,7 @@ impl fmt::Display for Command {
     }
 }
 
-fn encode_str(input: &str) -> String {
+fn encode(input: &String) -> String {
     let mut output = String::with_capacity(input.len());
 
     for c in input.chars() {
@@ -77,24 +77,24 @@ impl Header {
 
     pub fn add(&mut self, key: &str, value: &str) {
         self.map
-            .entry(encode_str(key))
+            .entry(key.to_string())
             .or_insert_with(|| Vec::with_capacity(1))
-            .push(encode_str(value));
+            .push(value.to_string());
     }
 
     pub fn set(&mut self, key: &str, values: Vec<&str>) {
         let mut c = Vec::with_capacity(values.len());
 
         for v in values {
-            c.push(encode_str(v));
+            c.push(v.to_string());
         }
 
         self.map
-            .insert(encode_str(key), c);
+            .insert(key.to_string(), c);
     }
 
     pub fn remove(&mut self, key: &str) {
-        self.map.remove(encode_str(key).as_str());
+        self.map.remove(key);
     }
 
     pub fn write_to<T: Write>(&self, w: &mut T) -> io::Result<usize> {
@@ -102,7 +102,7 @@ impl Header {
         let mut bytes_written = 0;
 
         for (k, v) in self.map.iter() {
-            let field_str = format!("{}: {}\n", k, v.join(","));
+            let field_str = format!("{}: {}\n", encode(k), encode(&v.join(",")));
             let size = bw.write(field_str.as_bytes())?;
             bytes_written += size;
         }
@@ -125,29 +125,43 @@ fn write_header() {
 }
 
 #[test]
+fn write_header_colon() {
+    let target = "Content-Type: vnd\\capplication/json\nContent-Length: 30\n";
+
+    let mut header = Header::new();
+    header.add("Content-Type", "vnd:application/json");
+    header.add("Content-Length", "30");
+
+    let mut buffer: Vec<u8> = Vec::new();
+    header.write_to(&mut buffer).unwrap();
+    let data = str::from_utf8(&buffer).unwrap();
+    assert_eq!(target, data)
+}
+
+#[test]
 fn encode_backslash() {
-    let input = "Hello\\World";
-    let target = "Hello\\\\World";
-    assert!(target == encode_str(input))
+    let input = "Hello\\World".to_string();
+    let target = "Hello\\\\World".to_string();
+    assert_eq!(target, encode(&input))
 }
 
 #[test]
 fn encode_carriage_return() {
-    let input = "Hello\rWorld";
-    let target = "Hello\\rWorld";
-    assert!(target == encode_str(input))
+    let input = "Hello\rWorld".to_string();
+    let target = "Hello\\rWorld".to_string();
+    assert_eq!(target, encode(&input))
 }
 
 #[test]
 fn encode_newline() {
-    let input = "Hello\nWorld";
-    let target = "Hello\\nWorld";
-    assert!(target == encode_str(input))
+    let input = "Hello\nWorld".to_string();
+    let target = "Hello\\nWorld".to_string();
+    assert_eq!(target, encode(&input))
 }
 
 #[test]
 fn encode_semicolon() {
-    let input = "Hello:World";
-    let target = "Hello\\cWorld";
-    assert!(target == encode_str(input))
+    let input = "Hello:World".to_string();
+    let target = "Hello\\cWorld".to_string();
+    assert_eq!(target, encode(&input))
 }
